25dc6c707e4439ed13605211b0b1f9acd94a74b0
kosatun@aol.com <kosatun@aol.com>
2018-07-12 03:14:05 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -6,8 +6,8 @@ def turn_count(board)
  end
 end
 
-def current_player(board) do
-  if turn_count(board) % 2 == 0 do
+def current_player(board) 
+  if turn_count(board) % 2 == 0 
     return "X"
   elsif
     return "O"
