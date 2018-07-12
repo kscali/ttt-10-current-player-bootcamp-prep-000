88dc64ba5d9280f579b383a1c22f48314253ffc8
kosatun@aol.com <kosatun@aol.com>
2018-07-12 03:29:55 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,17 +1,1 @@
-def turn_count(board) 
- turn = 0 
- while turn < board.length do
-   if board[turn] == "X" || board[turn] == "O"
-   turn += 1
-   end
-   return turn
-end
 
-def current_player(board) 
-  if turn_count(board) % 2 == 0 
-    return "X"
-  else
-    return "O"
-  end  
- end
-end
